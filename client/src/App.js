2368b2d4eed044f3c7b46091563b8d66723134dc
Seun Adetunji <oluwaseunus@gmail.com>
2019-04-16 11:06:22 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import CreateContact from './CreateContact';
+import ListContacts from './components/ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+
+class App extends Component {
+	state = {
+		screen: 'list',
+		contacts: []
+	};
+
+	componentDidMount() {
+		ContactsAPI.getAll().then(contacts => {
+			this.setState({ contacts });
+		});
+	}
+
+	removeContact = contact => {
+		this.setState(state => ({
+			contacts: state.contacts.filter(c => c.id !== contact.id)
+		}));
+
+		ContactsAPI.remove(contact);
+	};
+
+	createContact = contact => {
+		ContactsAPI.create(contact).then(contact => {
+			this.setState(state => ({
+				contacts: [...state.contacts, contact]
+			}));
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<Route
+					exact
+					path='/'
+					render={() => (
+						<ListContacts
+							contacts={this.state.contacts}
+							onDeleteContact={this.removeContact}
+						/>
+					)}
+				/>
+
+				<Route
+					path='/create'
+					render={({ history }) => (
+						<CreateContact
+							onCreateContact={contact => {
+								this.createContact(contact);
+								history.push('/');
+							}}
+						/>
+					)}
+				/>
+			</div>
+		);
+	}
+}
+
+export default App;
